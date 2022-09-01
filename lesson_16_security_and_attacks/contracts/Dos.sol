261b9235ce68ff1c4a14bfe92c892e298dbf80ca
Maksim <maksim.kalashynski@gmail.com>
2022-09-01 23:36:56 +0300

    Dos attack

--- lesson_16_security_and_attacks/contracts/Dos.sol
+++ lesson_16_security_and_attacks/contracts/Dos.sol
@@ -6,18 +6,25 @@ contract DosAuction {
     mapping(address => uint) public bidders;
     address[] public allBidders;
     uint public refundProgress;
-
+//    address[] public failedBidders;
     function bid() external payable {
+//        3- вариант, не 100% защита
+//        require(msg.sender.code.length==0 , "no contracts");
         bidders[msg.sender] += msg.value;
         allBidders.push(msg.sender);
     }
     //    push
+//    Решение проблемы -1) исп pull чтобы user сам делал запрос для выплачивания денег
     function refund() external {
         for (uint i = refundProgress; i < allBidders.length; i++) {
             address bidder = allBidders[i];
 
             (bool success,) = bidder.call{value : bidders[bidder]}("");
             require(success, "failed!");
+//            2-й вариант, доп массив где хранятся невыполненые адреса
+//            if (!success){
+//                failedBidders.push(bidder);
+//            }
 
             refundProgress++;
         }
